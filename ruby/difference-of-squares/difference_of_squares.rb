16f4c4f68451fd7290236a5ec13b9088c99312df
Fotis Alexandrou <falexandrou@gmail.com>
2018-03-19 11:55:54 +0200

    difference of squares exercise

---
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,23 @@
+module BookKeeping
+  VERSION = 1
+end
+
+class Squares
+  @num = 0
+
+  def initialize(num)
+    @num = num
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+
+  def square_of_sum
+    1.upto(@num).reduce(0, :+) ** 2
+  end
+
+  def sum_of_squares
+    1.upto(@num).map { |n| n ** 2 }.reduce(0, &:+)
+  end
+end
